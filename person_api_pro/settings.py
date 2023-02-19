4475b8dda14dc1579da8367330ae971a29e6142d
John Tovor <johntovor@gmail.com>
2023-02-19 13:47:37 +0000

    render deployment reconfigured

--- person_api_pro/settings.py
+++ person_api_pro/settings.py
@@ -12,27 +12,26 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-qxst4ika+on&+j&ese_t%a$o(#%rr2*6!^pgrvfx6838rp11(r'
-SECRET_KEY = os.environ.get('SECRET_KEY', default=')(98&*^%rTH*7^5qxst4ika+on&+j&ese_t%a$o(#%rr2*6!^pgrvfx6838rp11(r')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = []
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -95,11 +94,7 @@ WSGI_APPLICATION = 'person_api_pro.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/person_api_pro',
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
